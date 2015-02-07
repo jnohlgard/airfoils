1269805b98e62370ee6e7682df9c6dceb07803aa
Joakim Gebart <joakim.gebart@eistec.se>
2015-02-07 07:56:54 +0100

    svg: Add stroke attribute support.

--- airfoil/svg.py
+++ airfoil/svg.py
@@ -1,20 +1,26 @@
 default_style = "stroke: black; fill: none; stroke-width: 0.2"
 
-__all__ = ["spline", "smooth_spline"]
-spline_template = '<path style="{style}" d="{path}" />'
-polyline_template = '<polyline style="{style}" points="{points}" />'
+__all__ = ["spline", "polyline"]
+spline_template = '<path style="{style}" d="{path}"{stroke} />'
+polyline_template = '<polyline style="{style}" points="{points}"{stroke} />'
 
-def polyline(x, y, style=None):
+def polyline(x, y, style=None, stroke=None):
     if style is None:
         style = default_style
+
+    if stroke is not None:
+        stroke = ' stroke="{stroke}"'.format(stroke=stroke)
+    else:
+        stroke = ''
+
     points = '{x},{y}'.format(x=x[0], y=y[0])
     for i in range(1, len(x)):
         points += ' {x},{y}'.format(x=x[i], y=y[i])
-    polyline = polyline_template.format(points=points, style=style)
+    polyline = polyline_template.format(points=points, style=style, stroke=stroke)
     return polyline
 
 # See http://stackoverflow.com/questions/1257168/how-do-i-create-a-bezier-curve-to-represent-a-smoothed-polyline
-def smooth_spline(x, y, tension, style=None):
+def smooth_spline(x, y, tension, style=None, stroke=None):
     n = len(x);
     dx = list();
     dy = list();
@@ -28,9 +34,15 @@ def smooth_spline(x, y, tension, style=None):
 
     if style is None:
         style = default_style
+
+    if stroke is not None:
+        stroke = ' stroke="{stroke}"'.format(stroke=stroke)
+    else:
+        stroke = ''
+
     path = 'M {x},{y} C {bx},{by}'.format(x=x[0], y=y[0], bx=x[0]+dx[0], by=y[0]+dy[0])
     path += ' {bx},{by} {x},{y}'.format(x=x[1], y=y[1], bx=x[1]-dx[1], by=y[1]-dy[1])
     for i in range(2, len(x)):
         path += ' S {bx},{by} {x},{y}'.format(x=x[i], y=y[i], bx=x[i]-dx[i], by=y[i]-dy[i])
-    spline = spline_template.format(path=path, style=style)
+    spline = spline_template.format(path=path, style=style, stroke=stroke)
     return spline
